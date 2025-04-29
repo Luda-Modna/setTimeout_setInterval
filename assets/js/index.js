98a252e6396978bc60909e383559f7bab7296503
Luda Modna <ludmilamodna3@gmail.com>
2025-04-29 11:28:04 +0300

    Added simple timeout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,60 +1,14 @@
 "use strict";
 
-let time = new Date(0);
-let intervalId = null;
-
-const timeEl = document.querySelector(".time");
-const [startBtn, stopBtn, resetBtn] = document.querySelectorAll(
-  ".btn-container > button"
-);
-
-updateTime(time);
-
-function startBtnHandler() {
-  const DELAY = 10;
-  function tick() {
-    time.setMilliseconds(time.getMilliseconds() + DELAY);
-    updateTime(time);
-  }
-  if (!intervalId) {
-    intervalId = setInterval(tick, DELAY);
-  }
+function printNumbers(from, to, interval) {
+  let current = from;
+  const timeoutId = setInterval(() => {
+    console.log(current);
+    if (current === to) {
+      clearInterval(timeoutId);
+    }
+    current++;
+  }, interval);
 }
 
-startBtn.onclick = startBtnHandler;
-
-function stopBtnHandler() {
-  clearInterval(intervalId);
-  intervalId = null;
-}
-
-stopBtn.onclick = stopBtnHandler;
-
-function resetBtnHandler() {
-  time = new Date(0);
-  updateTime(time);
-}
-resetBtn.onclick = resetBtnHandler;
-
-function updateTime(time) {
-  timeEl.textContent = `
-    ${formatMinutesOrSeconds(time.getMinutes())}
-    :
-    ${formatMinutesOrSeconds(time.getSeconds())}
-    .
-    ${formatMilliseconds(time.getMilliseconds())}
-    `;
-}
-
-// форматуємо двозначні числа: якщо число з однієї цифри,
-// то додаємо попереду 0: 5 => 05
-function formatMinutesOrSeconds(m) {
-  return m < 10 ? `0${m}` : m;
-}
-
-// форматуємо тризначні числа:
-// якщо число з однієї цифри, то додаємо попереду 00:  5 => 005
-// якщо число з двох цифр, то додаємо попереду 0    : 55 => 055
-function formatMilliseconds(ms) {
-  return ms < 100 ? (ms < 10 ? `00${ms}` : `0${ms}`) : ms;
-}
+printNumbers(1, 8, 1000);
