a8554a350df37600a882607f77b3ce120c59061d
Luda Modna <ludmilamodna3@gmail.com>
2025-04-29 10:39:47 +0300

    Add simple stopwatch

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//let time = new Date(0);
+let time = 0;
+let intervalId = null;
+
+const timeEl = document.querySelector(".time");
+const [startBtn, stopBtn, resetBtn] = document.querySelectorAll(
+  ".btn-container > button"
+);
+
+function startBtnHandler() {
+  function tick() {
+    time++;
+    updateTime(time);
+  }
+  if (!intervalId) {
+    intervalId = setInterval(tick, 1);
+  }
+}
+
+startBtn.onclick = startBtnHandler;
+
+function stopBtnHandler() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+stopBtn.onclick = stopBtnHandler;
+
+function resetBtnHandler() {
+  time = 0;
+  updateTime(time);
+}
+resetBtn.onclick = resetBtnHandler;
+
+function updateTime(time) {
+  timeEl.textContent = time;
+}
